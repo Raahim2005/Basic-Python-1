e09a78321f09a2b7a076d87dbbfcc785c459b1da
Raahim2005 <72337192+Raahim2005@users.noreply.github.com>
2020-10-21 15:51:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,83 @@
+print("hello world")
+ex_var = 5
+ex_var1 = 7
+
+# Basic data types
+int_1 = 16
+float_1 = 1.234
+bool_1 = True
+
+# Variable and Assignment Exercise
+
+int = 5
+float = 14.0204
+bool = False
+int = 10
+
+# Comments and Basic Math Operators
+
+addition = 4 + 5            # 9
+subtraction = 5 - 2         # 3
+division = 7 / 2            # 3.5
+multiplaction = 3 * 8       # 24
+
+exponentiation = 4 ** 4     # 256
+floor_division = 16//5      # 3
+modulo = 7 % 3              # 1
+
+add_assign = 5
+add_assign = add_assign + 7
+add_assign += 7
+
+ # subtraction
+sub = 10
+sub -= 5    # 5
+ # multiplaction
+mult = 10
+mult *= 5   # 50
+ #division
+div = 10
+div /= 5    # 2
+ # exponentiation
+exp = 10
+exp **= 2   # 100
+ #floor division
+floor = 10
+floor //= 6 # 1
+ # modulo
+mod = 10
+mod %= 7    # 3
+
+#example
+print((9-7)* 2 **3 + 10 % 6 // -1 * 2 - 1)  # equation (7)
+
+# Print
+print(ex_var)
+print(int_1)
+print(division)
+print((4+5) / 2)
+
+# print exercise
+var = 50
+print(var)
+print(24)
+print(24 * 6 / 3)
+
+# Floats
+print(1.23 + 2.80)       # not accurate aprox error
+ex2 = (123 + 280) / 100  # no approx error
+print(ex2)
+print(round(ex2, 2))     # using round to eleminate aprox error
+
+# Programming Challenge: Grocery Store Purchase
+
+Penne     = 16.98
+Arrabiata = 6.98
+Cloves    = 16.78
+Bottle    = 15.26
+Baguettes = 3.00
+Meatballs = 4.32
+Subtotal = ( Penne + Arrabiata + Cloves + Bottle + Baguettes + Meatballs )
+print(round(Subtotal,0 ))
+
+print(addition + 5)
